70ec9839bed43eba41e4d1e35739759388bb04d8
smart8612 <smart8612@gmail.com>
2021-08-29 15:25:10 +0900

    Satisfied with this

--- Set/Views/Squiggle.swift
+++ Set/Views/Squiggle.swift
@@ -11,26 +11,30 @@ struct Squiggle: Shape {
     func path(in rect: CGRect) -> Path {
         var p = Path()
         
-        p.move(to: CGPoint(x: rect.midX * 0.2, y: rect.maxY))
+        p.move(to: CGPoint(x: rect.minX, y: rect.maxY * 0.5))
         p.addQuadCurve(
-            to: CGPoint(x: rect.midX * 1.2, y: rect.maxY * 0.25),
-            control: CGPoint(x: rect.origin.x, y: rect.midY * 0.25)
+            to: CGPoint(x: rect.midX, y: rect.maxY * 0.2),
+            control: CGPoint(x: rect.minX, y: rect.minY)
         )
         p.addQuadCurve(
-            to: CGPoint(x: rect.maxX * 0.75, y: rect.origin.y),
-            control: CGPoint(x: rect.midX * 1.25, y: rect.midY * 0.5)
+            to: CGPoint(x: rect.maxX * 0.8, y: rect.maxY * 0.15),
+            control: CGPoint(x: rect.maxX * 0.65, y: rect.maxY * 0.25)
         )
         p.addQuadCurve(
-            to: CGPoint(x: rect.maxX * 0.75, y: rect.maxY * 0.75),
-            control: CGPoint(x: rect.maxX, y: rect.midY)
+            to: CGPoint(x: rect.maxX, y: rect.maxY * 0.2),
+            control: CGPoint(x: rect.maxX * 0.98, y: rect.minY)
         )
         p.addQuadCurve(
-            to: CGPoint(x: rect.midX * 0.8, y: rect.maxY * 0.75),
-            control: CGPoint(x: rect.maxX * 0.6, y: rect.maxY)
+            to: CGPoint(x: rect.midX, y: rect.maxY * 0.65),
+            control: CGPoint(x: rect.maxX, y: rect.maxY)
         )
         p.addQuadCurve(
-            to: CGPoint(x: rect.midX * 0.2, y: rect.maxY),
-            control: CGPoint(x: rect.midX * 0.4, y: rect.maxY * 0.75)
+            to: CGPoint(x: rect.maxX * 0.18, y: rect.maxY * 0.7),
+            control: CGPoint(x: rect.maxX * 0.3, y: rect.maxY * 0.5)
+        )
+        p.addQuadCurve(
+            to: CGPoint(x: rect.minX, y: rect.maxY * 0.5),
+            control: CGPoint(x: rect.minX, y: rect.maxY)
         )
 
         
@@ -42,9 +46,9 @@ struct Squiggle: Shape {
 struct Squiggle_Previews: PreviewProvider {
     static var previews: some View {
         VStack(spacing: 0) {
+            Squiggle().padding()
             Squiggle()
-            Squiggle()
-            Squiggle()
+            Squiggle().stroke(lineWidth: 5)
         }
     }
 }
