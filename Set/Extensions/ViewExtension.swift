245ff163ecc68cc24565f401bde2eb561f8f9ef7
smart8612 <smart8612@gmail.com>
2021-08-30 14:56:54 +0900

    constructing cards code design

--- Set/Extensions/ViewExtension.swift
+++ Set/Extensions/ViewExtension.swift
@@ -8,8 +8,17 @@
 import Foundation
 import SwiftUI
 
+/*
+ View와 관련된 프로토콜에 대하여 코드의 중복을 줄이기 위해
+ 별도의 extension으로 한번만 구현해두고 재사용 하려고 함
+ */
 extension Shape {
-    func stripe(numberOfLines: Int, borderWidth: CGFloat) -> some View {
+    /// set shape background to stripe pattern
+    /// - Parameters:
+    ///   - numberOfLines: the number of background lines (default: 100)
+    ///   - borderWidth: shape border width (default: 25)
+    /// - Returns: some View
+    func stripe(numberOfLines: Int = 100, borderWidth: CGFloat = 25) -> some View {
         Stripe(numberOfStripes: numberOfLines)
             .clipShape(self)
             .overlay(self
--- Set/Views/SetGameView.swift
+++ Set/Views/SetGameView.swift
@@ -10,12 +10,22 @@ import SwiftUI
 struct SetGameView: View {
     var body: some View {
         HStack {
-            CardView()
+            CardView(
+                cardCount: .three,
+                cardShape: .diamond,
+                cardShading: .striped,
+                cardColor: .blue)
+                .foregroundColor(.red)
         }
     }
 }
 
 struct CardView: View {
+    var cardCount: CardCount
+    var cardShape: CardShape
+    var cardShading: CardShading
+    var cardColor: CardColor
+    
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 25)
@@ -23,16 +33,44 @@ struct CardView: View {
             RoundedRectangle(cornerRadius: 25)
                 .strokeBorder(lineWidth: 5)
             VStack(spacing: 0) {
-//                stripeDiamond.aspectRatio(2, contentMode: .fit)
-//                Diamond().stripe(numberOfLines: 100, borderWidth: 25)
-//                Circle().stripe(numberOfLines: 100, borderWidth: 25)
+                ForEach(0..<cardCount.rawValue) { _ in
+                    Circle().stripe()
+                        .aspectRatio(2, contentMode: .fit)
+                        .padding()
+                }
             }
         }
+        .foregroundColor(cardColor.rawValue)
+    }
+    
+    
+    /// Cards Domain Count
+    enum CardCount: Int {
+        case one = 1, two, three
+    }
+    
+    /// Card's Domain Shape
+    enum CardShape {
+        case squiggle, diamond, oval
+    }
+    
+    /// Card's Domain Shading
+    enum CardShading {
+        case solid, striped, open
+    }
+    
+    /// Card's Domain Color
+    struct CardColor {
+        let rawValue: Color
+        static let red = CardColor(rawValue: .red)
+        static let green = CardColor(rawValue: .green)
+        static let blue = CardColor(rawValue: .blue)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         SetGameView()
+            .preferredColorScheme(.dark)
     }
 }
